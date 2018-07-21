52498a64658c7bac848024bfb23ab8eee1a23649
ntecrc <gonzalezrc@gmail.ccom>
2018-07-21 11:52:09 -0400

    added products

--- schema.sql
+++ schema.sql
@@ -7,7 +7,10 @@ USE bamazon;
 
 /* Create new table with a primary key that auto-increaments, and a text field*/
 CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price decimal(10, 4) NOT NULL,
+    stock_quantity INT(10, 4) NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ seeds.sql
@@ -0,0 +1,15 @@
+/* Seeds for SQL table.*/
+USE bamazon;
+
+/* Insert 3 Rows into new table */
+INSERT item_id (name)
+VALUES ("UID for each product");
+
+INSERT INTO productS (name)
+(product_name, department_name, price, stock_quantity)
+VALUES
+("Nike Fx", "Footwear", 145, 8),
+("Mouse pad", "Computer Accessories", 2, 5),
+("Nintendo Switch", "Gaming", 500, 20),
+("Fender Stratocaster", "Instruments", 1500, 10),
+("2L 2% Milk jug", "Food", 3, 6),
